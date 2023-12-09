c43fa670c47c2081fb1b16dcb45c6dec19bbd0c8
jmaffa <90526195+jmaffa@users.noreply.github.com>
2023-12-09 02:20:21 -0500

    TODO: need to setup wifi, then need to make sure post is formatted correctly, then need to make sure the webapp is able to handle from the arduino, look at line 27 in server.js

--- websitev2/app.js
+++
@@ -1,6 +0,0 @@
-const { SerialPort } = require('serialport')
-const { ReadlineParser } = require('@serialport/parser-readline')
-const port = new SerialPort({ path: '/dev/cu.usbmodem21201', baudRate: 14400 })
-
-const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
-parser.on('data', console.log)
--- websitev2/post.php
+++
@@ -1,7 +0,0 @@
-<?php
-$time = time();
-$brain = $_POST['brain'];
-$file = 'brain.html';
-$data = $time. " - ".$brain;
-file_put_contents($file, $data);
-?>
